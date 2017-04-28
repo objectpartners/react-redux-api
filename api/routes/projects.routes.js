8374eca78862391047fdef8a05456d76ddb76707
scottbock <l.scott.bock@gmail.com>
2017-04-28 12:09:12 -0500

    attempt to work around cors errors

--- api/routes/projects.routes.js
+++ api/routes/projects.routes.js
@@ -4,15 +4,40 @@ var projectsController = require('../controllers/projects.controller');
 
 exports.register = function (server, options, next) {
 
-  var projects =  '/projects';
+  var projects = '/projects';
   var projectId = '/projects/{projectId}';
 
   server.route([
-    {method: 'GET',     path: projects, handler: projectsController.index},
-    {method: 'POST',    path: projects, handler: projectsController.create},
-    {method: 'GET',     path: projectId, handler: projectsController.show},
-    {method: 'PUT',     path: projectId, handler: projectsController.update},
-    {method: 'DELETE',  path: projectId, handler: projectsController.destroy}
+    {
+      method: 'GET',
+      path: projects,
+      handler: projectsController.index,
+      config: {auth: false, cors: {origin: ['*'], credentials: true}}
+    },
+    {
+      method: 'POST',
+      path: projects,
+      handler: projectsController.create,
+      config: {auth: false, cors: {origin: ['*'], credentials: true}}
+    },
+    {
+      method: 'GET',
+      path: projectId,
+      handler: projectsController.show,
+      config: {auth: false, cors: {origin: ['*'], credentials: true}}
+    },
+    {
+      method: 'PUT',
+      path: projectId,
+      handler: projectsController.update,
+      config: {auth: false, cors: {origin: ['*'], credentials: true, methods: ['PUT']}}
+    },
+    {
+      method: 'DELETE',
+      path: projectId,
+      handler: projectsController.destroy,
+      config: {auth: false, cors: {origin: ['*'], credentials: true}}
+    }
   ]);
 
   return next();
